bd23f85b0db47f3b6a9c82291aa9a95df021ac05
Hugo <hugo.andreas@totvs.com.br>
2021-07-05 19:16:41 -0300

    fix(): some errors from deps and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import React from 'react';
+
 function App() {
-  return (
-    <h1>A long time ago, in a galaxy far, far away...</h1>
-  );
+  return <h1>A long time ago, in a galaxy far, far away...</h1>;
 }
 
 export default App;
