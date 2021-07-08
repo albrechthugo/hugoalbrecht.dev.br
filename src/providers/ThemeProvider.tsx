cc60c45e983bf07d0b21d71bfea6d5d1d4fcec2a
Hugo <hugo.andreas@totvs.com.br>
2021-07-08 18:53:49 -0300

    refactor(): move directory

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-import { themes } from '../shared/themes/themes';
+import { themes } from '../shared/mocks/themes';
 
 type Props = {
   children: React.ReactNode;
