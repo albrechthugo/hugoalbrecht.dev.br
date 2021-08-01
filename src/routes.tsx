0a2092c9ad9a96f9d2d8574a7fea0aa0d7f327e0
Hugo <albrechthugo4@gmail.com>
2021-08-01 14:54:54 -0300

    feat(): add lazy loading to app

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Container from './components/Container';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
 
 export const Routes = (): JSX.Element => {
+  const Home = lazy(() => import('./pages/Home/'));
+  const Projects = lazy(() => import('./pages/Projects/'));
+
   return (
     <Router>
       <Switch>
-        <Container>
-          <Route exact path="/" component={Home} />
-          <Route path="/projects" component={Projects} />
-        </Container>
+        <Suspense fallback={<h1>Carregando...</h1>}>
+          <Container>
+            <Route exact path="/" component={Home} />
+            <Route path="/projects" component={Projects} />
+          </Container>
+        </Suspense>
       </Switch>
     </Router>
   );
