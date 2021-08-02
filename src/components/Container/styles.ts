0d411ff5726a83a067ab6e8764833ebeb81ac064
Hugo <albrechthugo4@gmail.com>
2021-08-02 20:10:12 -0300

    refactor(): using rem

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -5,13 +5,13 @@ export const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
   display: grid;
-  grid-template-columns: 80px auto;
+  grid-template-columns: 8rem auto;
   grid-template-rows: 100%;
   grid-template-areas: 'sidebar main';
 
   @media (max-width: 1000px) {
     grid-template-columns: 100%;
-    grid-template-rows: auto 80px;
+    grid-template-rows: auto 8rem;
     grid-template-areas: 'main' 'sidebar';
   }
 `;
