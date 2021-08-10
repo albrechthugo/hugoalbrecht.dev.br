7818e3d69a1182f26cd4c78a2a8d303324fd1866
Hugo <albrechthugo4@gmail.com>
2021-08-10 18:53:07 -0300

    stage

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+  ],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleNameMapper: {
+    '^styled-components':
+      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js',
+  },
+};
