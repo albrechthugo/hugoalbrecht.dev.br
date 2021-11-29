d9c8f22355ccb208dcb5d0ef38072f6f07a22f04
Hugo <albrechthugo4@gmail.com>
2021-11-29 11:24:27 -0300

    refactor: using object key to acess property instead switch case

--- src/hooks/http/useHttp.ts
+++ src/hooks/http/useHttp.ts
@@ -3,26 +3,30 @@ import api from '../../services/api';
 
 export enum HttpMethods {
   POST = 'POST',
-  GET = 'DELETE',
-  DELETE = 'GET',
+  GET = 'GET',
+  DELETE = 'DELETE',
   PUT = 'PUT',
 }
+
+const methods: {
+  [key in HttpMethods]: <T>(
+    url: string,
+    options: any
+  ) => Promise<AxiosResponse<T>>;
+} = {
+  POST: (url: string, options: any) => api.post(url, options),
+  GET: (url: string, options: any) => api.get(url, options),
+  DELETE: (url: string, options: any) => api.delete(url, options),
+  PUT: (url: string, options: any) => api.put(url, options),
+};
 
 const useHttp = <T>(
   url: string,
   method: HttpMethods,
   options?: any
 ): Promise<AxiosResponse<T>> => {
-  switch (method) {
-    case HttpMethods.GET:
-      return api.get(url, options);
-    case HttpMethods.POST:
-      return api.post(url, options);
-    case HttpMethods.DELETE:
-      return api.delete(url, options);
-    case HttpMethods.PUT:
-      return api.put(url, options);
-  }
+  console.log(method);
+  return methods[method]<T>(url, options);
 };
 
 export default useHttp;
