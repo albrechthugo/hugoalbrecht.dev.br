b734018f3241112e89de1d6c80e9dac60af648dd
Hugo <albrechthugo4@gmail.com>
2021-11-29 11:25:08 -0300

    update: test dependencies and description

--- src/assets/i18n/translate.ts
+++ src/assets/i18n/translate.ts
@@ -1,8 +1,8 @@
 const literals = {
   ptBr: {
     name: 'Hugo Andreas Albrecht',
-    description: `Sou desenvolvedor front-end com cerca de 1 ano de experiência trabalhando com o ecossistema Javascript,
-      atualmente faço parte do time da TOTVS, utilizando Angular e Typescript no desenvolvimento de softwares SaaS.
+    description: `Sou engenheiro de software com foco no frontend e com experiência trabalhando com o ecossistema Javascript,
+      atualmente faço parte do PicPay, utilizando Angular e Typescript no desenvolvimento de softwares SaaS.
       Continuo meus estudos com outras ferramentas que usam Javascript como coração: React, styled-components entre outros.`,
     chooseLanguage: 'Escolha o idioma abaixo:',
     english: 'Inglês',
@@ -15,9 +15,9 @@ const literals = {
   },
   enUs: {
     name: 'Hugo Andreas Albrecht',
-    description: `I'm a front-end developer with about 1 year of experience working with the Javascript ecosystem,
-      I'm currently part of the TOTVS team, using Angular and Typescript in SaaS software development.
-      I continue my studies with other tools that use Javascript as their heart: React, styled-components among others.`,
+    description: `I'm a frontend-focused software engineer with experience working with the Javascript ecosystem,
+    I'm currently part of PicPay, using Angular and Typescript in SaaS software development.
+    I continue my studies with other tools that use Javascript as their heart: React, styled-components, among others.`,
     chooseLanguage: 'Choose the language below:',
     english: 'English',
     espanhol: 'Spanish',
@@ -29,9 +29,9 @@ const literals = {
   },
   es: {
     name: 'Hugo Andreas Albrecht',
-    description: `Soy un desarrollador front-end con aproximadamente un año de experiencia trabajando con el ecosistema de Javascript,
-      Actualmente soy parte del equipo de TOTVS, usando Angular y Typecript en el desarrollo de software SaaS.
-      Continúo mis estudios con otras herramientas que usan Javascript como corazón: React, styled-components, entre otros.`,
+    description: `Soy un ingeniero de software centrado en el frontend con experiencia trabajando con el ecosistema de Javascript,
+    Actualmente soy parte de PicPay, usando Angular y Typecript en el desarrollo de software SaaS.
+    Continúo mis estudios con otras herramientas que usan Javascript como su corazón: React, styled-components, entre otras.`,
     chooseLanguage: 'Elija el idioma a continuación:',
     english: 'Inglés',
     espanhol: 'Español',
